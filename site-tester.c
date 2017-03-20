c466cf2e1d769a9136f0bab7d0ef697f9b75b427
Nick Palutsis <npalutsi@nd.edu>
2017-03-20 19:04:52 -0400

    Reads in a configuration file and prints it out. Still need to parse the file

--- site-tester.c
+++ site-tester.c
@@ -1,135 +1,136 @@
-/***************************************************************************
- *                                  _   _ ____  _
- *  Project                     ___| | | |  _ \| |
- *                             / __| | | | |_) | |
- *                            | (__| |_| |  _ <| |___
- *                             \___|\___/|_| \_\_____|
- *
- * Copyright (C) 1998 - 2015, Daniel Stenberg, <daniel@haxx.se>, et al.
- *
- * This software is licensed as described in the file COPYING, which
- * you should have received as part of this distribution. The terms
- * are also available at https://curl.haxx.se/docs/copyright.html.
- *
- * You may opt to use, copy, modify, merge, publish, distribute and/or sell
- * copies of the Software, and permit persons to whom the Software is
- * furnished to do so, under the terms of the COPYING file.
- *
- * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
- * KIND, either express or implied.
- *
- ***************************************************************************/
-/* <DESC>
- * Shows how the write callback function can be used to download data into a
- * chunk of memory instead of storing it in a file.
- * </DESC>
+/* Project 4: System for Verifying Web Placement
+ * Authors: Nick Palutsis & Travis Gayle
+ * Date: March 24, 2017
  */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
 #include <curl/curl.h>
 
-const char *configFile = sys.argv[1]; // one specified argument for config file using syntax "PARAM=XXXXXX"
-
-// default value for param
-int PERIOD_FETCH = 180 //The time (in seconds) between fetches of the various sites
-int NUM_FETCH = 1 //Number of fetch threads
-int NUM_PARSE = 1 //Number of parsing threads
-char *SEARCH_FILE = "Search.txt"	//File containing the search strings
-char *SITE_FILE = "Sites.txt"	//File containing the sites to query
-
-//queue that stores newly downloaded webpages to be processed by consumers
-
-//MAX NUMBER OF THREADS FOR DATA PROCESSING
-
-//max number of threads available for fetching
-
-// 1 thread each
-
-// Control C or SIGHUP for operation termination
-
-
 struct MemoryStruct {
-  char *memory;
-  size_t size;
+	char *memory;
+	size_t size;
 };
 
 static size_t
-WriteMemoryCallback(void *contents, size_t size, size_t nmemb, void *userp)
-{
-  size_t realsize = size * nmemb;
-  struct MemoryStruct *mem = (struct MemoryStruct *)userp;
+WriteMemoryCallback(void *contents, size_t size, size_t nmemb, void *userp) {
+	size_t realsize = size * nmemb;
+	struct MemoryStruct *mem = (struct MemoryStruct *)userp;
 
-  mem->memory = realloc(mem->memory, mem->size + realsize + 1);
-  if(mem->memory == NULL) {
-    /* out of memory! */
-    printf("not enough memory (realloc returned NULL)\n");
-    return 0;
-  }
+	mem->memory = realloc(mem->memory, mem->size + realsize + 1);
+	if(mem->memory == NULL) {
+		/* out of memory! */
+		printf("not enough memory (realloc returned NULL)\n");
+		return 0;
+	}
 
-  memcpy(&(mem->memory[mem->size]), contents, realsize);
-  mem->size += realsize;
-  mem->memory[mem->size] = 0;
+	memcpy(&(mem->memory[mem->size]), contents, realsize);
+	mem->size += realsize;
+	mem->memory[mem->size] = 0;
 
-  return realsize;
+	return realsize;
 }
 
-int main(void)
-{
-  CURL *curl_handle;
-  CURLcode res;
+int main(int argc, char *argv[]) {
+	if (argc == 2) {
+		// const char *configFile = argv[1]; // one specified argument for config file using syntax "PARAM=XXXXXX"
 
-  struct MemoryStruct chunk;
+        // We assume argv[1] is a filename to open
+        FILE *file = fopen( argv[1], "r" );
 
-  chunk.memory = malloc(1);  /* will be grown as needed by the realloc above */
-  chunk.size = 0;    /* no data at this point */
+        /* fopen returns 0, the NULL pointer, on failure */
+        if ( file == 0 ) {
+            printf( "Could not open file\n" );
+        } else {
+            int x;
+            /* read one character at a time from file, stopping at EOF, which
+               indicates the end of the file.  Note that the idiom of "assign
+               to a variable, check the value" used below works because
+               the assignment statement evaluates to the value assigned. */
+            while  ( ( x = fgetc( file ) ) != EOF ) {
+                printf( "%c", x );
+            }
+            fclose( file );
+        }		// Parse configFile and assign values to variables
 
-  curl_global_init(CURL_GLOBAL_ALL);
 
-  /* init the curl session */
-  curl_handle = curl_easy_init();
+		// int PERIOD_FETCH = 180; //The time (in seconds) between fetches of the various sites
+		// int NUM_FETCH = 1; //Number of fetch threads
+		// int NUM_PARSE = 1; //Number of parsing threads
+		// char *SEARCH_FILE = "Search.txt"; //File containing the search strings
+		// char *SITE_FILE = "Sites.txt";  //File containing the sites to query
+	} else {
+		// default value for param
+		int PERIOD_FETCH = 180; //The time (in seconds) between fetches of the various sites
+		int NUM_FETCH = 1; //Number of fetch threads
+		int NUM_PARSE = 1; //Number of parsing threads
+		char *SEARCH_FILE = "Search.txt"; //File containing the search strings
+		char *SITE_FILE = "Sites.txt";  //File containing the sites to query
+	}
 
-  /* specify URL to get */
-  curl_easy_setopt(curl_handle, CURLOPT_URL, "https://www.nd.edu");
 
-  /* send all data to this function  */
-  curl_easy_setopt(curl_handle, CURLOPT_WRITEFUNCTION, WriteMemoryCallback);
+	//queue that stores newly downloaded webpages to be processed by consumers
 
-  /* we pass our 'chunk' struct to the callback function */
-  curl_easy_setopt(curl_handle, CURLOPT_WRITEDATA, (void *)&chunk);
+	//MAX NUMBER OF THREADS FOR DATA PROCESSING
 
-  /* some servers don't like requests that are made without a user-agent
-     field, so we provide one */
-  curl_easy_setopt(curl_handle, CURLOPT_USERAGENT, "libcurl-agent/1.0");
+	//max number of threads available for fetching
 
-  /* get it! */
-  res = curl_easy_perform(curl_handle);
+	// 1 thread each
 
-  /* check for errors */
-  if(res != CURLE_OK) {
-    fprintf(stderr, "curl_easy_perform() failed: %s\n",
-            curl_easy_strerror(res));
-  }
-  else {
-    /*
-     * Now, our chunk.memory points to a memory block that is chunk.size
-     * bytes big and contains the remote file.
-     *
-     * Do something nice with it!
-     */
+	// Control C or SIGHUP for operation termination
 
-    printf("%lu bytes retrieved\n", (long)chunk.size);
-  }
+	CURL *curl_handle;
+	CURLcode res;
 
-  /* cleanup curl stuff */
-  curl_easy_cleanup(curl_handle);
+	struct MemoryStruct chunk;
 
-  free(chunk.memory);
+	chunk.memory = malloc(1);  /* will be grown as needed by the realloc above */
+	chunk.size = 0;    /* no data at this point */
 
-  /* we're done with libcurl, so clean it up */
-  curl_global_cleanup();
+	curl_global_init(CURL_GLOBAL_ALL);
 
-  return 0;
+	/* init the curl session */
+	curl_handle = curl_easy_init();
+
+	/* specify URL to get */
+	curl_easy_setopt(curl_handle, CURLOPT_URL, "https://www.nd.edu");
+
+	/* send all data to this function  */
+	curl_easy_setopt(curl_handle, CURLOPT_WRITEFUNCTION, WriteMemoryCallback);
+
+	/* we pass our 'chunk' struct to the callback function */
+	curl_easy_setopt(curl_handle, CURLOPT_WRITEDATA, (void *)&chunk);
+
+	/* some servers don't like requests that are made without a user-agent
+	 field, so we provide one */
+	curl_easy_setopt(curl_handle, CURLOPT_USERAGENT, "libcurl-agent/1.0");
+
+	/* get it! */
+	res = curl_easy_perform(curl_handle);
+
+	/* check for errors */
+	if(res != CURLE_OK) {
+		fprintf(stderr, "curl_easy_perform() failed: %s\n",
+				curl_easy_strerror(res));
+	} else {
+		/*
+		 * Now, our chunk.memory points to a memory block that is chunk.size
+		 * bytes big and contains the remote file.
+		 *
+		 * Do something nice with it!
+		 */
+
+		printf("%lu bytes retrieved\n", (long)chunk.size);
+	}
+
+	/* cleanup curl stuff */
+	curl_easy_cleanup(curl_handle);
+
+	free(chunk.memory);
+
+	/* we're done with libcurl, so clean it up */
+	curl_global_cleanup();
+
+	return 0;
 }
